1d5fa900ea03424f3510b0cca3a294a4f30c729e
Baek Sohee <soheesause@sookmyung.ac.kr>
2023-04-09 23:01:49 +0900

    [깃 실습 과제] 프론트엔드 개발 8 React에서 리스트 렌더링하기

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import Todo from './Todo';
+import {Paper, List}from "@material-ui/core";
 import './App.css';
 
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      item :{id:0, title:"Hello World 1", done:true},
+      items :[
+        {id:0, title:"Todo 1", done:true},
+        {id:1, title:"Todo 2", done:false},
+      ],
     };
   }
   render(){
-    return (
-      <div className='App'>
-        <Todo item={this.state.item}/>
-      </div>
+    var todoItems = this.state.items.length > 0 && (
+      <Paper style={{margin: 16}}>
+        <List>
+        {this.state.items.map((item, idx)=>(
+        <Todo item={item} key={item.id}/>
+        ))}
+        </List>
+      </Paper>
     );
+    
+    
+    return <div className='App'>{todoItems}</div>;
   }
 }
 
