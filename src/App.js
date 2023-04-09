b99581125963efd9cfc983080b188a71a95fff39
Baek Sohee <soheesause@sookmyung.ac.kr>
2023-04-09 23:29:11 +0900

    [깃 실습 과제] 프론트엔드 개발 9 React 이벤트 핸들러로 목록 추가하기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Todo from './Todo';
-import {Paper, List}from "@material-ui/core";
+import AddTodo from './AddTodo';
+import {Paper, List, Container}from "@material-ui/core";
 import './App.css';
 
 class App extends React.Component {
@@ -13,6 +14,16 @@ class App extends React.Component {
       ],
     };
   }
+
+  add = (item) => {
+    const thisItems = this.state.items;
+    item.id = "ID-"+ thisItems.length;
+    item.done = false;
+    thisItems.push(item);
+    this.setState({items:thisItems});
+    console.log("items:",this.state.items);
+  }
+
   render(){
     var todoItems = this.state.items.length > 0 && (
       <Paper style={{margin: 16}}>
@@ -25,7 +36,14 @@ class App extends React.Component {
     );
     
     
-    return <div className='App'>{todoItems}</div>;
+    return (
+    <div className='App'>
+      <Container maxWidth="md">
+        <AddTodo add={this.add}/>
+        <div className='TodoList'>{todoItems}</div>
+      </Container>
+    </div>
+    );
   }
 }
 
