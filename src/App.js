082f5a3354ef501c11e0089e7f2abd358beddcc5
Baek Sohee <soheesause@sookmyung.ac.kr>
2023-04-09 22:31:32 +0900

    [깃 실습 과제] 프론트엔드 개발 7 Props, state 사용하기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import Hello from './Hello';
+import Todo from './Todo';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      item :{id:0, title:"Hello World 1", done:true},
+    };
+  }
   render(){
     return (
       <div className='App'>
-        <Hello />
+        <Todo item={this.state.item}/>
       </div>
     );
   }
